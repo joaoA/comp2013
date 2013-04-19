e710cbdb48cbe1c16fa748adeb83924a6bae032c
Joao Amsellem <joao.amsellem@gmail.com>
2013-04-19 14:01:42 +0100

    structures.h changed

--- meta2/structures.h
+++ meta2/structures.h
@@ -2,25 +2,62 @@
 #define _STRUCTURES_
 
 
+typedef enum {functionDefinition, functionDeclaration,declaration} disc_start;
+typedef enum {d_intlit, d_charlit, d_strlit, d_id, , d_int , d_char, d_pointer } disc_terminal;
+typedef enum {is_or, is_and, is_eq, is_ne, is_lt, is_gt, is_le, is_ge, is_add, is_sub, is_mul, is_div, is_mod, is_not, is_minus, is_plus, is_addr, id_deref, is_store, is_call, is_print, is_atoi, is_itoa }is_operator;
+
+
+
+/*is_expression*/
+typedef struct _s1{
+	disc_terminal d_terminal;
+	union{
+		// adicionar o que falta
+		int number;
+		char character;	
+		char *string;
+		struct is_infix_expression *u_infix_exp;
+		struct is_unary_expression *u_unary_exp;
+	}data_expression;
+
+
+}is_expression;
+
+/*is_infix_expression*/
+typedef struct _s2{
+	is_expression *expression1;
+	is_operator oprtor;
+	is_expression *expression2;
+}is_infix_expression;
+
+/*is_unary_expression*/
+typedef struct _s3{
+	is_operator oprtor;
+	is_expression *expression1;
+}is_unary_expression;
+
+
+
+
 
 /*is_new_twelve*/
-typedef struct _s1{
+typedef struct _s{
 }is_new_twelve;
 
 /*is_declaration*/
-typedef struct _s1{
+typedef struct _s{
 }is_declaration;
 
 
 
 /*is_functionDeclaration;*/
-typedef struct _s1{
+typedef struct _s{
 }is_functionDeclaration;
 
 
 
 /*is_functionDefinition*/
-typedef struct _s1{
+typedef struct _s{
 }is_functionDefinition;
 
 
@@ -29,10 +66,8 @@ typedef struct _s1{
 /*****					start 					   *****/
 /*******************************************************/
 
-typedef enum {functionDefinition, functionDeclaration,declaration} disc_start;
-
 /*is_start*/
-typedef struct _s1{
+typedef struct _s{
 	disc_start disc_s;		/* functionDefinition || functionDeclaration || declaration */
 	is_new_twelve *isNewt;
 	union{
