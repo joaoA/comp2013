70f85012e368a8d82567e3e4d7904d0e93c5808f
Joao Amsellem <joao.amsellem@gmail.com>
2013-04-21 06:55:46 +0100

    functions adicionadas

--- meta2/functions.c
+++ meta2/functions.c
@@ -20,3 +20,66 @@ is_expression* insert_STRLIT(char *string)
 	ie->data_expression.string=string;	//Guarda o conteúdo
 	return ie;		
 }
+
+
+
+is_start* insert_start(int opt,is_start *list, is_declaration *declaration , is_func_declaration *func_declaration, is_func_definition *func_definition)
+{
+	/* por enquanto isto fica comentado até se irem fazendo as funçoes para baixo */
+
+	is_start *is=(is_start*)malloc(sizeof(is_start));
+	is_start* aux;
+	is->next=NULL;
+
+	if(opt==0){
+		is_declaration *id = (is_declaration*)malloc(sizeof(is_declaration));
+		/*id=insert_declartion(declaration);*/
+		printf("\ndeclaration\n\n");
+
+		is->d_start=d_declaration;
+		// is->data_start.u_declaration = id;
+		if(list!=NULL){
+			for(aux=list; aux->next!=NULL; aux=aux->next); /*procura ultimo no' e adiciona*/
+			aux->next=is;
+		}
+		else
+			return is;   /* caso seja o primeiro e unico elemento da lista  */
+
+
+	}
+	else if (opt==1){
+		is_func_definition *ifd = (is_func_definition*)malloc(sizeof(is_func_definition));
+		// ifd=insert_func_definition(func_definition);
+		printf("\nfunc_definition\n\n");
+
+		is->d_start=d_func_definition;
+		// is->data_start.u_func_definition = ifd;
+		if(list!=NULL){
+			for(aux=list; aux->next!=NULL; aux=aux->next); /*procura ultimo no' e adiciona*/
+			aux->next=is;
+		}
+		else
+			return is;		/* caso seja o primeiro e unico elemento da lista  */
+
+	}
+	else if(opt==2){
+		is_func_declaration *ifdec = (is_func_declaration*)malloc(sizeof(is_func_declaration));
+		// ifdec=insert_func_declartion(func_declaration);
+		printf("\nfunc_declaration\n\n");
+
+		is->d_start=d_func_declaration;
+		// is->data_start.u_func_declaration = ifdec;
+		if(list!=NULL){
+			for(aux=list; aux->next!=NULL; aux=aux->next); /*procura ultimo no' e adiciona*/
+			aux->next=is;
+		}
+		else
+			return is;		/* caso seja o primeiro e unico elemento da lista  */
+
+	}
+
+	return list;
+
+
+
+}
--- meta2/functions.h
+++ meta2/functions.h
@@ -3,3 +3,5 @@
 
 is_expression* insert_INTLIT(int);
 is_expression* insert_STRLIT(char*);
+
+is_start* insert_start(int opt, is_start *list, is_declaration *declaration , is_func_declaration *func_declaration, is_func_definition *func_definition);
